129a1c2644ee01d44c5402c107e112e9c3bdad5e
Anadudev <anadudev@gmail.com>
2025-03-18 06:27:09 +0100

    initialize project with Next.js, Tailwind CSS, and Liveblocks integration

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Room } from "@/app/Room";
+import { CollaborativeApp } from "@/app/CollaborativeApp";
+
+export default function Home() {
+  return (
+    <Room>
+      <CollaborativeApp />
+    </Room>
+  );
+}
