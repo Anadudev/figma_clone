bf7db05831a10d1b0a6d73acc4330274f4bd6e47
Anadudev <anadudev@gmail.com>
2025-03-21 16:34:28 +0100

    feat: add cursor chat functionality and manage cursor state in Live component

--- constants/Live.tsx
+++ constants/Live.tsx
@@ -1,11 +1,16 @@
 "use client";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import LiveCursors from "./cursor/LiveCursors";
 import { useOthers } from "@liveblocks/react";
 import { useMyPresence } from "@liveblocks/react/suspense";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/types";
+
 const Live = () => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence();
+  const [cursorState, setCursorState] = useState({mode:CursorMode.Hidden});
+
   const handlePointerMove = useCallback((e: React.PointerEvent) => {
     e.preventDefault();
     const x = e.clientX - e.currentTarget.getBoundingClientRect().x;
@@ -34,6 +39,7 @@ const Live = () => {
       onPointerDown={handlePointerDown}
     >
       <h1 className="text-2xl">Figma clone</h1>
+      {cursor && <CursorChat cursor={cursor} />}
 
       <LiveCursors others={others} />
     </div>
